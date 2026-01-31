8fdfa49f45af45f090ab98223e6772dc5a35dd56
Jimmy Lin <sirjimmylin@duck.com>
2026-01-31 01:01:45 -0500

    add navigation bar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,19 @@
 class ApplicationController < ActionController::Base
-  # Make the current user accessible to views and controllers
+  # Make the authenticated? method available to views (.erb files)
+  helper_method :authenticated?
+
   before_action :set_current_user
 
   private
+
   def set_current_user
     if session[:user_id]
       Current.user = User.find_by(id: session[:user_id])
     end
+  end
+
+  # Define what "authenticated?" actually means
+  def authenticated?
+    Current.user.present?
   end
 end
