8a02d5dd4f34dd23f5eff10b38cffc177afec739
Jimmy Lin <sirjimmylin@duck.com>
2026-01-31 00:25:40 -0500

    Update site configuration (Clean)

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,10 @@ Rails.application.routes.draw do
   get "search", to: "search#index"
   resources :posts, only: [:index, :show]
   resources :projects, only: [:index, :show]
+
+  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
+  # Can be used by load balancers and uptime monitors to verify that the app is live.
+  get "up" => "rails/health#show", as: :rails_health_check
   
   # --- ADD THIS LINE ---
   resources :contacts, only: [:create]
