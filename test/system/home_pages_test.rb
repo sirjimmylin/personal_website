e2aef5a1f7661d93f10ecf0e94c8815a348d242e
Jimmy Lin <sirjimmylin@duck.com>
2026-05-19 22:05:13 -0400

    Fix whitespace

--- test/system/home_pages_test.rb
+++ test/system/home_pages_test.rb
@@ -3,7 +3,7 @@ require "application_system_test_case"
 class HomePagesTest < ApplicationSystemTestCase
   test "visiting the home page" do
     visit root_url
-  
+
     assert_selector "h1" # We just check if there's any h1
   end
 end
